1bdb353ff2d880f9ed7eb91e5eb0954ebd42e67e
Souvik Ghosh <souvik.ghosh@rootquotient.com>
2026-05-07 18:31:09 +0530

    feat: add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Login from './components/auth/login'
 
-function App() {
+function Home() {
   const [count, setCount] = useState(0)
 
   return (
@@ -116,6 +118,17 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
+  )
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth/login" element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
